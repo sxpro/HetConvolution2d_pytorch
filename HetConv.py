5256d77b4480ebb5e5103f541afaac01fd14a639
sxpro <helloxprosuper@gmail.com>
2019-04-25 22:03:19 +0800

    updata hetconv_v2

--- HetConv.py
+++ HetConv.py
@@ -31,4 +31,45 @@ class HetConv2d(nn.Module):
             out.append(out_)
         return torch.cat(out, 1)
 
+class HetConv2d_v2(nn.Module):
+    def __init__(self, in_feats, out_feats, p=4, groups=1):
+        super(HetConv2d_v2, self).__init__()
+        if in_feats % groups != 0:
+            raise ValueError('in_channels must be divisible by groups')
+        self.p = p
+        self.in_feats = in_feats
+        self.out_feats = out_feats
+        self.groups = groups
+        self.blocks = nn.ModuleList()
+        for i in range(out_feats):
+            self.blocks.append(self.make_HetConv2d(p))
+
+    def make_HetConv2d(self, p):
+        layers = nn.ModuleList()
+        layers.append(nn.Conv2d(self.in_feats//p, self.in_feats//p, 3, 1, 1, groups=self.in_feats//p))
+        layers.append(nn.Conv2d(self.in_feats-self.in_feats//p, self.in_feats-self.in_feats//p, 1, 1, 0, groups=self.in_feats-self.in_feats//p))
+        return layers
+
+    def forward(self, x):
+        out = []
+        for i in range(0, self.out_feats):
+            out1x1 = torch.zeros([0])
+            out3x3 = torch.zeros([0])
+            for j in range(0, self.in_feats):
+                if ((j - i) % (self.p)) == 0:
+                    if out3x3.shape[0] == 0:
+                        out3x3 = x[:, j:j + 1, :, :]
+                    else:
+                        out3x3 = torch.cat((out3x3, x[:, j:j + 1, :, :]), 1)
+                else:
+                    if out1x1.shape[0] == 0:
+                        out1x1 = x[:, j:j + 1, :, :]
+                    else:
+                        out1x1 = torch.cat((out1x1, x[:, j:j + 1, :, :]), 1)
+            out3x3 = self.blocks[i][0](out3x3)
+            out1x1 = self.blocks[i][1](out1x1)
+            out_ = out1x1.sum(dim=1, keepdim=True) + out3x3.sum(dim=1, keepdim=True)
+            out.append(out_)
+        return torch.cat(out, 1)
+
 
